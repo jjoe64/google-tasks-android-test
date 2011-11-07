87199afb76e2fc30c28147d8f398e05faaedc954
Jonas Gehring <jonas.gehring@inovex.de>
2011-11-07 11:20:04 +0100

    erweiterte tests, update,delete

--- src/com/example/googletasks/test/content/TasksContentProviderTest.java
+++ src/com/example/googletasks/test/content/TasksContentProviderTest.java
@@ -2,14 +2,58 @@ package com.example.googletasks.test.content;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.test.ProviderTestCase2;
 
 import com.example.googletasks.content.TaskModel;
 import com.example.googletasks.content.TasksContentProvider;
 
 public class TasksContentProviderTest extends ProviderTestCase2<TasksContentProvider> {
+	private static final String TEST_NAME = "der Test";
+	private static final String TEST_NOTES = "bla blub";
+	private static final String TEST_DUE_DATE = "2011-11-30";
+
 	public TasksContentProviderTest() {
 		super(TasksContentProvider.class, "com.example.googletasks.contentprovider");
+	}
+
+	public TaskModel _insert() {
+		int done = 1;
+
+		ContentValues values = new ContentValues(4);
+		values.put(TaskModel.COLUMN_DONE, done);
+		values.put(TaskModel.COLUMN_NAME, TEST_NAME);
+		values.put(TaskModel.COLUMN_NOTES, TEST_NOTES);
+		values.put(TaskModel.COLUMN_DUE_DATE, TEST_DUE_DATE);
+
+		Uri uri = getMockContentResolver().insert(TasksContentProvider.CONTENT_URI, values);
+		Cursor c = getMockContentResolver().query(uri, null, null, null, null);
+		c.moveToFirst();
+		TaskModel mdl = TaskModel.parse(c);
+		c.close();
+
+		return mdl;
+	}
+
+	public void testDelete() {
+		TaskModel mdl = _insert();
+
+		// anzahl vorher
+		Cursor cursor = getMockContentResolver().query(
+				TasksContentProvider.CONTENT_URI
+				, null, null, null, null
+		);
+		int nVorher = cursor.getCount();
+
+		// löschen
+		getMockContentResolver().delete(Uri.withAppendedPath(TasksContentProvider.CONTENT_URI, String.valueOf(mdl.getId())), null, null);
+
+		// anzahl danach
+		cursor = getMockContentResolver().query(
+				TasksContentProvider.CONTENT_URI
+				, null, null, null, null
+		);
+		assertEquals(nVorher-1, cursor.getCount());
 	}
 
 	public void testInsert() {
@@ -20,18 +64,7 @@ public class TasksContentProviderTest extends ProviderTestCase2<TasksContentProv
 		);
 		int nVorher = cursor.getCount();
 
-		int done = 1;
-		String name ="der Test";
-		String notes = "bla blub";
-		String dueDate = "2011-11-30";
-
-		ContentValues values = new ContentValues(4);
-		values.put(TaskModel.COLUMN_DONE, done);
-		values.put(TaskModel.COLUMN_NAME, name);
-		values.put(TaskModel.COLUMN_NOTES, notes);
-		values.put(TaskModel.COLUMN_DUE_DATE, dueDate);
-
-		getMockContentResolver().insert(TasksContentProvider.CONTENT_URI, values);
+		TaskModel mdl = _insert();
 
 		// anzahl nachher / differenz
 		cursor = getMockContentResolver().query(
@@ -40,6 +73,31 @@ public class TasksContentProviderTest extends ProviderTestCase2<TasksContentProv
 		);
 		assertEquals(1, cursor.getCount()-nVorher);
 
-		// notified?
+		// daten prüfen
+		assertTrue(mdl.isDone());
+		assertEquals(TEST_DUE_DATE, mdl.getDueDate());
+		assertEquals(TEST_NAME, mdl.getName());
+		assertEquals(TEST_NOTES, mdl.getNotes());
+	}
+
+	public void testUpdate() {
+		TaskModel mdl = _insert();
+		long id = mdl.getId();
+
+		Uri uri = Uri.withAppendedPath(TasksContentProvider.CONTENT_URI, String.valueOf(id));
+		ContentValues values = new ContentValues(2);
+		values.put(TaskModel.COLUMN_NAME, "neuer Name");
+		values.put(TaskModel.COLUMN_NOTES, "neue Notizen");
+
+		getMockContentResolver().update(uri, values, null, null);
+
+		// requery
+		Cursor c = getMockContentResolver().query(uri, null, null, null, null);
+		c.moveToFirst();
+		mdl = TaskModel.parse(c);
+
+		// assert
+		assertEquals("neuer Name", mdl.getName());
+		assertEquals("neue Notizen", mdl.getNotes());
 	}
 }
