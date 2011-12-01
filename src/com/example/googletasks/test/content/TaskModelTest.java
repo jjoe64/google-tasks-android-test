c01cf02fcd252868ecfa538de06ecefe5aee859f
Jonas Gehring <jonas.gehring@inovex.de>
2011-12-01 20:46:44 +0100

    model test

--- src/com/example/googletasks/test/content/TaskModelTest.java
+++ src/com/example/googletasks/test/content/TaskModelTest.java
@@ -1,11 +1,57 @@
 package com.example.googletasks.test.content;
 
+import java.util.Calendar;
+
 import junit.framework.TestCase;
 import android.database.MatrixCursor;
 
 import com.example.googletasks.content.TaskModel;
 
 public class TaskModelTest extends TestCase {
+	public void testIsOverdue() {
+		MatrixCursor cursor = new MatrixCursor(
+				new String[] {
+						TaskModel.COLUMN__ID
+						, TaskModel.COLUMN_DONE
+						, TaskModel.COLUMN_DUE_DATE
+						, TaskModel.COLUMN_NAME
+						, TaskModel.COLUMN_NOTES
+				}
+		);
+		TaskModel mdl;
+		long _id = 1;
+		int done = 1;
+		String name = "der Test";
+		String notes = "testen testen testen";
+		String dueDate;
+
+		// ohne due date
+		dueDate = null;
+		cursor.addRow(new Object[] {_id, done, dueDate, name, notes});
+		cursor.moveToLast();
+		mdl = TaskModel.parse(cursor);
+		assertFalse(mdl.isOverdue());
+
+		// mit due date in der zukunft
+		Calendar now = Calendar.getInstance();
+		// heute +1 tag
+		now.add(Calendar.HOUR_OF_DAY, 24);
+		dueDate = now.get(Calendar.YEAR)+"-"
+			+ (now.get(Calendar.MONTH)<9?"0":"") + (now.get(Calendar.MONTH)+1)
+			+ "-" + (now.get(Calendar.DAY_OF_MONTH)<10?"0":"") + now.get(Calendar.DAY_OF_MONTH);
+		cursor.addRow(new Object[] {_id, done, dueDate, name, notes});
+		cursor.moveToLast();
+		mdl = TaskModel.parse(cursor);
+		assertFalse(mdl.isOverdue());
+
+		// mit due date in der vergangenheit
+		dueDate = "2011-01-01";
+		cursor.addRow(new Object[] {_id, done, dueDate, name, notes});
+		cursor.moveToLast();
+		mdl = TaskModel.parse(cursor);
+		assertTrue(mdl.isOverdue());
+	}
+
 	public void testParse() {
 		MatrixCursor cursor = new MatrixCursor(
 				new String[] {
